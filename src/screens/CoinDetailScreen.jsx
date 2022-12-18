d67ad3149ea0b87dcf37df763e979d65e4d434c8
Johnson-GitHub-New <64173435+Johnson-GitHub-New@users.noreply.github.com>
2022-12-19 10:44:56 +1100

    Upload

---
+++ src/screens/CoinDetailScreen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const CoinDetailScreen = () => {
+  return (
+    <View>
+      <Text>CoinDetailScreen</Text>
+    </View>
+  );
+};
+
+export default CoinDetailScreen;
+
+const styles = StyleSheet.create({});
